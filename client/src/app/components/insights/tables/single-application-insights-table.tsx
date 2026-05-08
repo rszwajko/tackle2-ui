52751e0b9e93204bd76b29450823280a1d38e7af
Radoslaw Szwajkowski <rszwajko@redhat.com>
2026-05-08 15:24:02 +0200

    Codemode 23-24
    Codemod 23: emptyStateHeader-move-into-emptyState (32 hits)
    Codemod 24: pageSection-update-variant-values (35 hits)
    
    Signed-off-by: Radoslaw Szwajkowski <rszwajko@redhat.com>

--- client/src/app/components/insights/tables/single-application-insights-table.tsx
+++ client/src/app/components/insights/tables/single-application-insights-table.tsx
@@ -270,12 +270,7 @@ export const SingleApplicationInsightsTable: React.FC<
           isError={!!fetchError}
           isNoData={totalItemCount === 0 || selectedAppId === null}
           noDataEmptyState={
-            <EmptyState variant="sm">
-              <EmptyStateHeader
-                titleText={t("message.selectApplicationFromFilterMenu")}
-                icon={<EmptyStateIcon icon={CubesIcon} />}
-                headingLevel="h2"
-              />
+            <EmptyState  headingLevel="h2" icon={CubesIcon}  titleText={t("message.selectApplicationFromFilterMenu")} variant="sm">
               <EmptyStateBody>
                 {t("message.selectApplicationFromFilterMenuDescription")}
               </EmptyStateBody>
